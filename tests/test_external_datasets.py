9daf071f95bae2a46a8f62a810c45a61e6b9b30a
Matthias Kümmerer <matthias@matthias-k.org>
2014-11-30 23:56:31 +0100

    First version of Toronto Code (does not yet check cache)
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

---
+++ tests/test_external_datasets.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import, print_function, division
+
+import unittest
+import os.path
+from six.moves import cPickle
+import dill
+
+import numpy as np
+from scipy.misc import imsave
+
+import pysaliency
+import pysaliency.external_datasets
+from test_helpers import TestWithData
+
+
+class TestExternalDatasets(TestWithData):
+    def test_toronto_without_location(self):
+        stimuli, fixations = pysaliency.external_datasets.get_toronto(location=None)
+        self.assertIsInstance(stimuli, pysaliency.Stimuli)
+        self.assertNotIsInstance(stimuli, pysaliency.FileStimuli)
+
+        self.assertEqual(len(stimuli.stimuli), 120)
+        for n in range(len(stimuli.stimuli)):
+            self.assertEqual(stimuli.shapes[n], (511, 681, 3))
+            self.assertEqual(stimuli.sizes[n], (511, 681))
+
+    def test_toronto_with_location(self):
+        stimuli, fixations = pysaliency.external_datasets.get_toronto(location=self.data_path)
+        self.assertIsInstance(stimuli, pysaliency.Stimuli)
+        self.assertIsInstance(stimuli, pysaliency.FileStimuli)
+
+        self.assertEqual(len(stimuli.stimuli), 120)
+        for n in range(len(stimuli.stimuli)):
+            self.assertEqual(stimuli.shapes[n], (511, 681, 3))
+            self.assertEqual(stimuli.sizes[n], (511, 681))
+
+
+if __name__ == '__main__':
+    unittest.main()
