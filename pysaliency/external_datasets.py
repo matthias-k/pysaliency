d342d255ebd6ffc8d8c3217a823ce7669c3b52ae
Matthias Kümmerer <matthias@matthias-k.org>
2014-12-02 18:34:46 +0100

    Subset of MIT1003
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- pysaliency/external_datasets.py
+++ pysaliency/external_datasets.py
@@ -14,6 +14,7 @@ from scipy.io import loadmat
 from natsort import natsorted
 import dill
 from pkg_resources import resource_string
+from PIL import Image
 
 from .datasets import FileStimuli, Stimuli, Fixations
 from .utils import TemporaryDirectory, which
@@ -292,6 +293,9 @@ def get_toronto_with_subjects(location=None):
 
         fixations = Fixations.from_fixation_trains(train_xs, train_ys, train_ts, train_ns, train_subjects)
 
+    if location:
+        dill.dump(stimuli, open(os.path.join(location, 'stimuli.pydat'), 'wb'))
+        dill.dump(fixations, open(os.path.join(location, 'fixations.pydat'), 'wb'))
     return stimuli, fixations
 
 
@@ -299,7 +303,7 @@ def get_mit1003(location=None):
     """
     Loads or downloads and caches the MIT1003 dataset. The dataset
     consists of 1003 natural indoor and outdoor scenes of
-    sizse: max dim: 1024px, other dim: 405-1024px
+    sizes: max dim: 1024px, other dim: 405-1024px
     and the fixations of 15 subjects under
     free viewing conditions with 3 seconds presentation time.
 
@@ -311,6 +315,14 @@ def get_mit1003(location=None):
                      will be stored in the subdirectory `toronto` of
                      location and read from there, if already present.
     @return: Stimuli, Fixations
+
+    .. note::
+        This code needs a working matlab or octave installation as the original
+        matlab code by Judd et al. is used to extract the fixation from the
+        eyetracking data.
+
+        The first fixation of each fixation train is discarded as stated in the
+        paper (Judd et al. 2009).
 
     .. seealso::
 
@@ -428,4 +440,159 @@ def get_mit1003(location=None):
                 train_subjects.append(subject_id)
         fixations = Fixations.from_fixation_trains(xs, ys, ts, ns, train_subjects)
 
+    if location:
+        dill.dump(stimuli, open(os.path.join(location, 'stimuli.pydat'), 'wb'))
+        dill.dump(fixations, open(os.path.join(location, 'fixations.pydat'), 'wb'))
+    return stimuli, fixations
+
+
+def get_mit1003_onesize(location=None):
+    """
+    Loads or downloads and caches the subset of the MIT1003 dataset
+    used in "How close are we to understanding image-based
+    saliency" (http://arxiv.org/abs/1409.7686) and
+    "Deep Gaze I: Boosting Saliency Prediction with Feature Maps Trained
+    on ImageNet" (http://arxiv.org/abs/1411.1045).
+    The dataset conists of 463 natural indoor and outdoor scenes of
+    size 1024 x 768 px and the fixations of 15 subjects under
+    free viewing conditions with 3 seconds presentation time.
+
+    All fixations outside of the image are discarded. This includes
+    blinks.
+
+    @type  location: string, defaults to `None`
+    @param location: If and where to cache the dataset. The dataset
+                     will be stored in the subdirectory `toronto` of
+                     location and read from there, if already present.
+    @return: Stimuli, Fixations
+
+    .. note::
+        This code needs a working matlab or octave installation as the original
+        matlab code by Judd et al. is used to extract the fixation from the
+        eyetracking data.
+
+        The first fixation of each fixation train is discarded as stated in the
+        paper (Judd et al. 2009).
+
+    .. seealso::
+
+        Tilke Judd, Krista Ehinger, Fredo Durand, Antonio Torralba. Learning to Predict where Humans Look [ICCV 2009]
+
+        http://people.csail.mit.edu/tjudd/WherePeopleLook/index.html
+    """
+    if location:
+        location = os.path.join(location, 'MIT1003_onesize')
+        if os.path.exists(location):
+            stimuli = dill.load(open(os.path.join(location, 'stimuli.pydat'), 'rb'))
+            fixations = dill.load(open(os.path.join(location, 'fixations.pydat'), 'rb'))
+            return stimuli, fixations
+        os.makedirs(location)
+    with TemporaryDirectory(cleanup=True) as temp_dir:
+        download_and_check('http://people.csail.mit.edu/tjudd/WherePeopleLook/ALLSTIMULI.zip',
+                           os.path.join(temp_dir, 'ALLSTIMULI.zip'),
+                           '0d7df8b954ecba69b6796e77b9afe4b6')
+        download_and_check('http://people.csail.mit.edu/tjudd/WherePeopleLook/DATA.zip',
+                           os.path.join(temp_dir, 'DATA.zip'),
+                           'ea19d74ad0a0144428c53e9d75c2d71c')
+        download_and_check('http://people.csail.mit.edu/tjudd/WherePeopleLook/Code/DatabaseCode.zip',
+                           os.path.join(temp_dir, 'DatabaseCode.zip'),
+                           'd8e5e2b6ec827f4115ddbff59b0bdf1d')
+
+        # Stimuli
+        print('Creating stimuli')
+        f = zipfile.ZipFile(os.path.join(temp_dir, 'ALLSTIMULI.zip'))
+        namelist = f.namelist()
+        namelist = filter_files(namelist, ['.svn', '__MACOSX', '.DS_Store'])
+        f.extractall(temp_dir, namelist)
+
+        stimuli_src_location = os.path.join(temp_dir, 'ALLSTIMULI')
+        stimuli_target_location = os.path.join(location, 'stimuli') if location else None
+        images = glob.glob(os.path.join(stimuli_src_location, '*.jpeg'))
+        images = [os.path.split(img)[1] for img in images]
+        stimuli_filenames = sorted(images)  # I used sorted instead of natsorted at this time
+
+        def check_size(f):
+            img = Image.open(os.path.join(stimuli_src_location, f))
+            return img.size == (1024, 768)
+
+        stimuli_filenames = [s for s in stimuli_filenames if check_size(s)]
+
+        stimuli = create_stimuli(stimuli_src_location, stimuli_filenames, stimuli_target_location)
+
+        # Fixations
+
+        print('Creating fixations')
+        f = zipfile.ZipFile(os.path.join(temp_dir, 'DATA.zip'))
+        namelist = f.namelist()
+        namelist = filter_files(namelist, ['.svn', '__MACOSX', '.DS_Store'])
+        f.extractall(temp_dir, namelist)
+
+        f = zipfile.ZipFile(os.path.join(temp_dir, 'DatabaseCode.zip'))
+        namelist = f.namelist()
+        namelist = filter_files(namelist, ['.svn', '__MACOSX', '.DS_Store'])
+        f.extractall(temp_dir, namelist)
+
+        subjects = ['kae', 'jw', 'tmj', 'ems', 'zb', 'emb', 'hp', 'ajs', 'tu', 'jcw', 'krl', 'CNG', 'ff', 'po', 'ya']
+
+        with open(os.path.join(temp_dir, 'extract_fixations.m'), 'wb') as f:
+            f.write(resource_string(__name__, 'scripts/{}'.format('extract_fixations.m')))
+
+        cmds = []
+        # It is vital _not_ to store the extracted fixations in the main
+        # directory where matlab is running, as matlab will check the timestamp
+        # of all files in this directory very often. This leads to heavy
+        # performance penalties and would make matlab run for more than an
+        # hour.
+        out_path = 'extracted'
+        os.makedirs(os.path.join(temp_dir, out_path))
+        total_cmd_count = len(stimuli_filenames) * len(subjects)
+        for n, stimulus in enumerate(stimuli_filenames):
+            for subject_id, subject in enumerate(subjects):
+                subject_path = os.path.join('DATA', subject)
+                outfile = '{0}_{1}.mat'.format(stimulus, subject)
+                outfile = os.path.join(out_path, outfile)
+                cmds.append("fprintf('%d/%d\\n', {}, {});".format(n*len(subjects)+subject_id, total_cmd_count))
+                cmds.append("extract_fixations('{0}', '{1}', '{2}');".format(stimulus, subject_path, outfile))
+
+        print('Running original code to extract fixations. This can take some minutes.')
+        print('Warning: In the IPython Notebook, the output is shown on the console instead of the notebook.')
+        with open(os.path.join(temp_dir, 'extract_all_fixations.m'), 'w') as f:
+            for cmd in cmds:
+                f.write('{}\n'.format(cmd))
+
+        run_matlab_cmd('extract_all_fixations;', cwd=temp_dir)
+        xs = []
+        ys = []
+        ts = []
+        ns = []
+        train_subjects = []
+        for n, stimulus in enumerate(stimuli_filenames):
+            stimulus_size = stimuli.sizes[n]
+            for subject_id, subject in enumerate(subjects):
+                subject_name = os.path.split(subject)[-1]
+                outfile = '{0}_{1}.mat'.format(stimulus, subject_name)
+                mat_data = loadmat(os.path.join(temp_dir, out_path, outfile))
+                fix_data = mat_data['fixations']
+                starts = mat_data['starts']
+                x = []
+                y = []
+                t = []
+                for i in range(1, fix_data.shape[0]):  # Skip first fixation like Judd does
+                    if fix_data[i, 0] < 0 or fix_data[i, 1] < 0:
+                        continue
+                    if fix_data[i, 0] >= stimulus_size[1] or fix_data[i, 1] >= stimulus_size[0]:
+                        continue
+                    x.append(fix_data[i, 0])
+                    y.append(fix_data[i, 1])
+                    t.append(starts[0, i]/240.0)  # Eye Tracker rate = 240Hz
+                xs.append(x)
+                ys.append(y)
+                ts.append(t)
+                ns.append(n)
+                train_subjects.append(subject_id)
+        fixations = Fixations.from_fixation_trains(xs, ys, ts, ns, train_subjects)
+
+    if location:
+        dill.dump(stimuli, open(os.path.join(location, 'stimuli.pydat'), 'wb'))
+        dill.dump(fixations, open(os.path.join(location, 'fixations.pydat'), 'wb'))
     return stimuli, fixations
--- tests/test_external_datasets.py
+++ tests/test_external_datasets.py
@@ -2,11 +2,6 @@ from __future__ import absolute_import, print_function, division
 
 import unittest
 import os.path
-from six.moves import cPickle
-import dill
-
-import numpy as np
-from scipy.misc import imsave
 
 import pysaliency
 import pysaliency.external_datasets
@@ -28,6 +23,7 @@ class TestExternalDatasets(TestWithData):
         stimuli, fixations = pysaliency.external_datasets.get_toronto(location=self.data_path)
         self.assertIsInstance(stimuli, pysaliency.Stimuli)
         self.assertIsInstance(stimuli, pysaliency.FileStimuli)
+        self.assertTrue(os.path.isdir(os.path.join(self.data_path, 'toronto')))
 
         self.assertEqual(len(stimuli.stimuli), 120)
         for n in range(len(stimuli.stimuli)):
@@ -41,7 +37,7 @@ class TestExternalDatasets(TestWithData):
 
         self.assertEqual(len(stimuli.stimuli), 1003)
         for n in range(len(stimuli.stimuli)):
-            self.assertEqual(max(stimuli.sizes[n], 1024))
+            self.assertEqual(max(stimuli.sizes[n]), 1024)
 
         self.assertEqual(len(fixations.x), 104171)
         self.assertEqual((fixations.n == 0).sum(), 121)
@@ -50,14 +46,39 @@ class TestExternalDatasets(TestWithData):
         stimuli, fixations = pysaliency.external_datasets.get_mit1003(location=self.data_path)
         self.assertIsInstance(stimuli, pysaliency.Stimuli)
         self.assertIsInstance(stimuli, pysaliency.FileStimuli)
+        self.assertTrue(os.path.isdir(os.path.join(self.data_path, 'MIT1003')))
 
         self.assertEqual(len(stimuli.stimuli), 1003)
         for n in range(len(stimuli.stimuli)):
-            self.assertEqual(max(stimuli.sizes[n], 1024))
+            self.assertEqual(max(stimuli.sizes[n]), 1024)
 
         self.assertEqual(len(fixations.x), 104171)
         self.assertEqual((fixations.n == 0).sum(), 121)
 
+    def test_mit_onesize_without_location(self):
+        stimuli, fixations = pysaliency.external_datasets.get_mit1003_onesize(location=None)
+        self.assertIsInstance(stimuli, pysaliency.Stimuli)
+        self.assertNotIsInstance(stimuli, pysaliency.FileStimuli)
+
+        self.assertEqual(len(stimuli.stimuli), 463)
+        for n in range(len(stimuli.stimuli)):
+            self.assertEqual(stimuli.sizes[n], (768, 1024))
+
+        self.assertEqual(len(fixations.x), 48771)
+        self.assertEqual((fixations.n == 0).sum(), 121)
+
+    def test_mit_onesize_with_location(self):
+        stimuli, fixations = pysaliency.external_datasets.get_mit1003_onesize(location=self.data_path)
+        self.assertIsInstance(stimuli, pysaliency.Stimuli)
+        self.assertIsInstance(stimuli, pysaliency.FileStimuli)
+        self.assertTrue(os.path.isdir(os.path.join(self.data_path, 'MIT1003')))
+
+        self.assertEqual(len(stimuli.stimuli), 463)
+        for n in range(len(stimuli.stimuli)):
+            self.assertEqual(stimuli.sizes[n], (768, 1024))
+
+        self.assertEqual(len(fixations.x), 48771)
+        self.assertEqual((fixations.n == 0).sum(), 121)
 
 if __name__ == '__main__':
     unittest.main()
