72650e892708bb8d98929e9f05dfb993f33c9c2d
Matthias Kümmerer <matthias@matthias-k.org>
2014-12-12 18:58:26 +0100

    New tests for external models
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- tests/test_external_models.py
+++ tests/test_external_models.py
@@ -1,93 +1,100 @@
 from __future__ import absolute_import, print_function, division
 
+import os
+import shutil
 import unittest
+from itertools import product
+
+from nose.tools import assert_equal, nottest
 
 import numpy as np
 
 import pysaliency
 import pysaliency.external_datasets
-from test_helpers import TestWithData
 
 
-class TestExternalModels(TestWithData):
-    def test_AIM_without_location(self):
-        model = pysaliency.AIM()
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+@nottest
+def create_test_stimuli():
+    color_stimulus = np.load(os.path.join('tests', 'external_models', 'color_stimulus.npy'))
+    grayscale_stimulus = np.load(os.path.join('tests', 'external_models', 'grayscale_stimulus.npy'))
+    return color_stimulus, grayscale_stimulus
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
 
-    def test_AIM_with_location(self):
-        model = pysaliency.AIM(location=self.data_path)
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+class DataStore(object):
+    def __init__(self):
+        self.color_stimulus, self.grayscale_stimulus = create_test_stimuli()
+        self.old_matlab_names = pysaliency.utils.MatlabOptions.matlab_names
+        self.old_octave_names = pysaliency.utils.MatlabOptions.octave_names
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
 
-    def test_SUN_without_location(self):
-        model = pysaliency.SUN()
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+def setUp():
+    DataStore.ds = DataStore()
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
 
-    def test_SUN_with_location(self):
-        model = pysaliency.SUN(location=self.data_path)
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+class ModelTemplate(object):
+    data_path = 'test_data'
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    def setUp(self):
+        if os.path.isdir(self.data_path):
+            shutil.rmtree(self.data_path)
+        if not os.path.exists(self.data_path):
+            os.makedirs(self.data_path)
 
-    def test_ContextAwareSaliency_without_location(self):
-        model = pysaliency.ContextAwareSaliency()
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    def tearDown(self):
+        shutil.rmtree(self.data_path)
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    def get_model(self, location, matlab):
+        if matlab == 'octave':
+            pysaliency.utils.MatlabOptions.matlab_names = []
+            pysaliency.utils.MatlabOptions.octave_names = DataStore.ds.old_octave_names
+        else:
+            pysaliency.utils.MatlabOptions.matlab_names = DataStore.ds.old_matlab_names
+            pysaliency.utils.MatlabOptions.octave_names = []
+        model = self.create_model(location)
+        return model
 
-    def test_ContextAwareSaliency_with_location(self):
-        model = pysaliency.ContextAwareSaliency(location=self.data_path)
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    def create_model(self, location):
+        raise NotImplementedError()
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    @property
+    def params(self):
+        return product([None, self.data_path], ['matlab', 'octave'])
 
-    def test_BMS_without_location(self):
-        model = pysaliency.BMS()
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    def test_saliency_maps(self):
+        for location, matlab in self.params:
+            yield self.check_saliency_maps, location, matlab
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+    def check_saliency_maps(self, location, matlab):
+        model = self.get_model(location, matlab)
+        color_stimulus = DataStore.ds.color_stimulus
+        saliency_map = model.saliency_map(color_stimulus)
+        np.testing.assert_allclose(saliency_map,
+                                   np.load(os.path.join('tests', 'external_models', '{}_color_stimulus.npy'.format(model.__modelname__))))
+        grayscale_stimulus = DataStore.ds.grayscale_stimulus
+        saliency_map = model.saliency_map(grayscale_stimulus)
+        np.testing.assert_allclose(saliency_map,
+                                   np.load(os.path.join('tests', 'external_models', '{}_grayscale_stimulus.npy'.format(model.__modelname__))))
 
-    def test_BMS_with_location(self):
-        model = pysaliency.BMS(location=self.data_path)
-        stimulus = np.random.randn(600, 400, 3)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
 
-        stimulus = np.random.randn(600, 400)
-        saliency_map = model.saliency_map(stimulus)
-        self.assertEqual(saliency_map.shape, (600, 400))
+class TestAIM(ModelTemplate):
+    def create_model(self, location):
+        return pysaliency.AIM(location=location)
+
+
+class TestSUN(ModelTemplate):
+    def create_model(self, location):
+        return pysaliency.SUN(location=location)
+
+
+class TestContextAwareSaliency(ModelTemplate):
+    def create_model(self, location):
+        return pysaliency.ContextAwareSaliency(location=location)
+
+
+#class TestBMS(ModelTemplate):
+#    def create_model(self, location):
+#        return pysaliency.BMS(location=location)
+
+
 if __name__ == '__main__':
     unittest.main()
