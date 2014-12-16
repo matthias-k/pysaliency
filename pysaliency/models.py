6df2849294a2661ef43ea84d8dbd452eb173bf6b
Matthias Kümmerer <matthias@matthias-k.org>
2014-12-16 19:54:35 +0100

    Sampling
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- pysaliency/models.py
+++ pysaliency/models.py
@@ -6,6 +6,30 @@ import numpy as np
 
 import generics
 from .saliency_map_models import GeneralSaliencyMapModel, SaliencyMapModel, handle_stimulus
+from .datasets import FixationTrains
+
+
+def sample_from_image(densities, count=None):
+    height, width = densities.shape
+    sorted_densities = densities.flatten(order='C')
+    cumsums = np.cumsum(sorted_densities)
+    if count is None:
+        real_count = 1
+    else:
+        real_count = count
+    sample_xs = []
+    sample_ys = []
+    tmps = np.random.rand(real_count)
+    js = np.searchsorted(cumsums, tmps)
+    for j in js:
+        sample_xs.append(j % width)
+        sample_ys.append(j // width)
+    sample_xs = np.asarray(sample_xs)
+    sample_ys = np.asarray(sample_ys)
+    if count is None:
+        return sample_xs[0], sample_ys[0]
+    else:
+        return np.asarray(sample_xs), np.asarray(sample_ys)
 
 
 class GeneralModel(GeneralSaliencyMapModel):
@@ -35,6 +59,104 @@ class GeneralModel(GeneralSaliencyMapModel):
 
     def log_likelihood(self, stimuli, fixations):
         return np.mean(self.log_likelihoods(stimuli, fixations))
+
+    def _expand_sample_arguments(self, stimuli, train_counts, lengths=None, stimulus_indices=None):
+        if isinstance(train_counts, int):
+            train_counts = [train_counts]*len(stimuli)
+
+        if stimulus_indices is None:
+            if len(train_counts) > len(stimuli):
+                raise ValueError('Number of train counts higher than count of stimuli!')
+            stimulus_indices = range(len(train_counts))
+
+        if isinstance(stimulus_indices, int):
+            stimulus_indices = [stimulus_indices]
+
+        if len(stimulus_indices) != len(train_counts):
+            raise ValueError('Number of train counts must match number of stimulus_indices!')
+
+        if isinstance(lengths, int):
+            lengths = [lengths for i in range(len(stimuli))]
+
+        if len(train_counts) != len(lengths):
+            raise ValueError('Number of train counts and number of lengths do not match!')
+
+        new_lengths = []
+        for k, l in enumerate(lengths):
+            if isinstance(l, int):
+                ll = [l for i in range(train_counts[k])]
+            else:
+                ll = l
+            new_lengths.append(ll)
+        lengths = new_lengths
+
+        for k, (c, ls) in enumerate(zip(train_counts, lengths)):
+            if c != len(ls):
+                raise ValueError('{}th train count ({}) does not match given number of lengths ({})!'.format(k, c, len(ls)))
+
+        return stimuli, train_counts, lengths, stimulus_indices
+
+    def sample(self, stimuli, train_counts, lengths=1, stimulus_indices=None):
+        """
+        Sample fixations for given stimuli
+
+
+        Examples
+        --------
+
+        >>> model.sample(stimuli, 10)  # sample 10 fixations per image
+        >>> # sample 5 fixations from the first image, 10 from the second and
+        >>> # 30 fixations from the third image
+        >>> model.sample(stimuli, [5, 10, 30])
+        >>> # Sample 10 fixation trains per image, each consiting of 5 fixations
+        >>> model.sample(stimuli, 10, lengths=5)
+        >>> # Sample 10 fixation trains per image, consisting of 2 fixations
+        >>> # for the first image, 4 fixations for the second, ...
+        >>> model.sample(stimuli, 10, lengths=[2, 4, 3])
+        >>> # Sample
+        >>> model.sample(stimuli, 3, lengths=[[1,2,3], [1,2,3]])
+
+        >>> # Sample 3 fixations from the 10th stimulus
+        >>> model.sample(stimuli, 3, stimulus_indices = 10)
+
+        >>> # Sample 3 fixations from the 20th and the 42th stimuli each
+        >>> model.sample(stimuli, 3, stimulus_indices = [20, 42])
+        """
+
+        stimuli, train_counts, lengths, stimulus_indices = self._expand_sample_arguments(stimuli,
+                                                                                         train_counts,
+                                                                                         lengths,
+                                                                                         stimulus_indices)
+
+        xs = []
+        ys = []
+        ts = []
+        ns = []
+        subjects = []
+        for stimulus_index, ls in generics.progressinfo(zip(stimulus_indices, lengths)):
+            stimulus = stimuli[stimulus_index]
+            for l in ls:
+                this_xs, this_ys, this_ts = self._sample_fixation_train(stimulus, l)
+                xs.append(this_xs)
+                ys.append(this_ys)
+                ts.append(this_ts)
+                ns.append(stimulus_index)
+                subjects.append(0)
+        return FixationTrains.from_fixation_trains(xs, ys, ts, ns, subjects)
+
+    def _sample_fixation_train(self, stimulus, length):
+        """Sample one fixation train of given length from stimulus"""
+        xs = []
+        ys = []
+        ts = []
+        for i in range(length):
+            log_densities = self.conditional_log_density(stimulus, xs, ys, ts)
+            x, y = sample_from_image(np.exp(log_densities))
+            xs.append(x)
+            ys.append(y)
+            ts.append(len(ts))
+
+        return xs, ys, ts
 
 
 class Model(GeneralModel, SaliencyMapModel):
@@ -82,3 +204,12 @@ class Model(GeneralModel, SaliencyMapModel):
     def _saliency_map(self, stimulus):
         # We have to implement this abstract method
         pass
+
+    def _sample_fixation_train(self, stimulus, length):
+        """Sample one fixation train of given length from stimulus"""
+        # We could reuse the implementation from `GeneralModel`
+        # but this implementation is much faster for long trains.
+        log_densities = self.log_density(stimulus)
+        xs, ys = sample_from_image(np.exp(log_densities), count=length)
+        ts = np.arange(len(xs))
+        return xs, ys, ts
