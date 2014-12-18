e0ccb35dc0c5e26d96cbfce43a6ba2c0833cf889
Matthias Kümmerer <matthias@matthias-k.org>
2014-12-18 14:34:58 +0100

    Theano code for saliency map processing
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- pysaliency/theano_utils.py
+++ pysaliency/theano_utils.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, print_function, division, unicode_literals
 
+import numpy as np
+
 import theano
 import theano.tensor as T
+from theano.ifelse import ifelse
 
 
 def nonlinearity(input, x, y, length):
@@ -24,3 +27,114 @@ def nonlinearity(input, x, y, length):
     for part in parts:
         output = output + part
     return output
+
+
+def gaussian_filter(input, sigma, window_radius = 40):
+    """
+    Filter input with a Gaussian using mode `nearest`.
+
+    input is expected to be three dimensional of type n times x times y
+    """
+
+    # Construction of 1d kernel
+    #filter_1d = T.arange(-window_radius, window_radius+1)
+    # Work around some strange theano bug
+    filter_1d = T.arange(2*window_radius + 1) - window_radius
+
+    filter_1d = T.exp(-0.5*filter_1d**2/sigma**2)
+    filter_1d = filter_1d / filter_1d.sum()
+    filter_1d = filter_1d.astype(input.dtype)
+
+    W = filter_1d.dimshuffle([0, 'x'])
+    W2 = filter_1d.dimshuffle(['x', 0])
+
+    blur_input = input.dimshuffle(['x', 0, 1, 2])
+    filter_W = W.dimshuffle(['x', 'x', 0, 1])
+    filter_W2 = W2.dimshuffle(['x', 'x', 0, 1])
+
+    # Construction of filter pipeline
+    blur_input_start = blur_input[:, :, :1, :]
+    blur_input_end = blur_input[:, :, -1:, :]
+    padded_input = T.concatenate([blur_input_start]*window_radius+[blur_input]+[blur_input_end]*window_radius, axis=2)
+
+    blur_op = T.nnet.conv2d(padded_input, filter_W, border_mode='full', filter_shape=[1, 1, None, None])
+    x_min = (W.shape[1]-1)//2
+    x_max = input.shape[2]+(W.shape[1]-1)//2
+    y_min = (W.shape[0]-1)//2+window_radius
+    y_max = input.shape[1]+(W.shape[0]-1)//2+window_radius
+    cropped_output1 = blur_op[:, :, y_min:y_max, x_min:x_max]
+
+    cropped_output1_start = blur_op[:, :, y_min:y_max, x_min:x_min+1]
+    cropped_output1_end = blur_op[:, :, y_min:y_max, x_max-1:x_max]
+    padded_cropped_input = T.concatenate([cropped_output1_start]*window_radius
+                                         + [cropped_output1]
+                                         + [cropped_output1_end] * window_radius, axis=3)
+    blur_op2 = T.nnet.conv2d(padded_cropped_input, filter_W2, border_mode='full', filter_shape=[1, 1, None, None])
+    x_min2 = (W2.shape[1]-1)//2+window_radius
+    x_max2 = input.shape[2]+(W2.shape[1]-1)//2+window_radius
+    y_min2 = (W2.shape[0]-1)//2
+    y_max2 = input.shape[1]+(W2.shape[0]-1)//2
+    cropped_output2 = blur_op2[0, :, y_min2:y_max2, x_min2:x_max2]
+    return cropped_output2
+
+
+class Blur(object):
+    def __init__(self, input, sigma=20.0, window_radius=60):
+        self.input = input
+        self.sigma = theano.shared(value=np.array(sigma), name='sigma')
+        apply_blur = T.gt(self.sigma, 0.0)
+        self.output = ifelse(apply_blur, gaussian_filter(input.dimshuffle('x', 0, 1), self.sigma, window_radius)[0, :, :], input)
+        self.params = [self.sigma]
+
+
+class Nonlinearity(object):
+    def __init__(self, input, nonlinearity_ys = None):
+        self.input = input
+        #self.num_nonlinearity = num_nonlinearity
+        if nonlinearity_ys is None:
+            nonlinearity_ys = np.linspace(0, 1, num=20)
+        self.nonlinearity_xs = theano.shared(value=np.linspace(0, 1, len(nonlinearity_ys)), name='nonlinearity_xs')
+        self.nonlinearity_ys = theano.shared(value=nonlinearity_ys, name='nonlinearity_ys')
+        self.output = nonlinearity(input, self.nonlinearity_xs, self.nonlinearity_ys, len(nonlinearity_ys))
+        self.params = [self.nonlinearity_ys]
+
+
+class CenterBias(object):
+    def __init__(self, input, centerbias = None, alpha=1.0):
+        self.input = input
+        if centerbias is None:
+            centerbias = np.ones(12)
+        self.alpha = theano.shared(value = np.array(alpha), name='alpha')
+        self.centerbias_ys = theano.shared(value=centerbias, name='centerbias_ys')
+        self.centerbias_xs = theano.shared(value=np.linspace(0, 1, len(centerbias)), name='centerbias_xs')
+
+        height = T.cast(input.shape[0], 'float64')
+        width = T.cast(input.shape[1], 'float64')
+        x_coords = (T.arange(width) - 0.5*width) / (0.5*width)
+        y_coords = (T.arange(height) - 0.5*height) / (0.5*height) + 0.0001  # We cannot have zeros in there because of grad
+
+        x_coords = x_coords.dimshuffle('x', 0)
+        y_coords = y_coords.dimshuffle(0, 'x')
+
+        dists = T.sqrt(T.square(x_coords) + self.alpha*T.square(y_coords))
+        self.max_dist = T.sqrt(1 + self.alpha)
+        self.dists = dists/self.max_dist
+
+        self.factors = nonlinearity(self.dists, self.centerbias_xs, self.centerbias_ys, len(centerbias))
+
+        apply_centerbias = T.lt(self.centerbias_ys.shape[0], 2)
+        self.output = ifelse(apply_centerbias, self.input*self.factors, self.input)
+        self.params = [self.centerbias_ys, self.alpha]
+
+
+class LogDensity(object):
+    def __init__(self, input):
+        self.input = input
+        self.output = T.log(input / input.sum())
+
+
+class AverageLogLikelihood(object):
+    def __init__(self, log_densities, x_inds, y_inds):
+        self.log_densities = log_densities
+        self.log_likelihoods = log_densities[y_inds, x_inds]
+        self.average_log_likelihood = self.log_likelihoods.mean()
--- tests/test_theano_utils.py
+++ tests/test_theano_utils.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import, print_function, division, unicode_literals
 
 import unittest
+from itertools import product
 
 import numpy as np
+from scipy.ndimage import gaussian_filter as scipy_filter
 import theano
 import theano.tensor as T
 
-from pysaliency.theano_utils import nonlinearity
+from pysaliency.theano_utils import nonlinearity, gaussian_filter, CenterBias, Blur
 
 
 class TestNonlinearity(unittest.TestCase):
@@ -55,3 +57,135 @@ class TestNonlinearity(unittest.TestCase):
         inp = np.linspace(0, 1, self.length)
         out = self.f(inp, x, y)
         np.testing.assert_allclose(out, y)
+
+
+class TestBlur(object):
+    def setUp(self):
+        theano.config.compute_test_value = 'ignore'
+
+    def test_blur_zeros(self):
+        sigma = theano.shared(20.0)
+        window_radius = 20*4
+        data = T.tensor3('data', dtype='float64')
+        data.tag.test_value = np.zeros((1, 10, 10))
+
+        blur = gaussian_filter(data, sigma, window_radius)
+
+        f = theano.function([data], blur)
+
+        test_data = np.zeros((1000, 1000))
+        out = f(test_data[np.newaxis, :, :])[0, :, :]
+
+        np.testing.assert_allclose(out, 0)
+
+    def test_blur_ones(self):
+        sigma = theano.shared(20.0)
+        window_radius = 20*4
+        data = T.tensor3('data', dtype='float64')
+        data.tag.test_value = np.zeros((1, 10, 10))
+
+        blur = gaussian_filter(data, sigma, window_radius)
+
+        f = theano.function([data], blur)
+
+        test_data = np.ones((1000, 1000))
+        out = f(test_data[np.newaxis, :, :])[0, :, :]
+
+        np.testing.assert_allclose(out, 1)
+
+    def test_other(self):
+        dtypes = ['float64', 'float32']
+        inputs = ['pixel', 'random']
+        sigmas = [20.0]
+        for dtype, input, sigma in product(dtypes, inputs, sigmas):
+            yield self.check_other, dtype, input, sigma
+
+    def check_other(self, dtype, input, sigma):
+        theano.config.compute_test_value = 'ignore'
+        sigma_theano = theano.shared(sigma)
+        window_radius = int(sigma*4)
+        if input == 'pixel':
+            test_data = np.ones((100, 100))
+            test_data[50, 50] = 2
+        elif input == 'random':
+            test_data = 10*np.ones((100, 100))
+            test_data += np.random.randn(100, 100)
+
+        else:
+            raise ValueError(input)
+
+        test_data = test_data.astype(dtype)
+        data = T.tensor3('data', dtype=dtype)
+        data.tag.test_value = test_data[np.newaxis, :, :]
+        print(data.dtype)
+
+        blur = gaussian_filter(data, sigma_theano, window_radius)
+
+        f = theano.function([data], blur)
+        out = f(test_data[np.newaxis, :, :])[0, :, :]
+
+        scipy_out = scipy_filter(test_data, sigma, mode='nearest')
+
+        if dtype == 'float32':
+            rtol = 1e-6
+        else:
+            rtol = 1e-7
+        np.testing.assert_allclose(out, scipy_out, rtol=rtol)
+
+
+class TestBlurObject(object):
+    def test_blur(self):
+        theano.config.floatX = 'float64'
+        data = T.matrix('data')
+        data.tag.test_value = np.random.randn(10, 10)
+        blur = Blur(data, sigma=20.0, window_radius = 80)
+
+        tmp = np.random.randn(1000, 2000)
+        tmp += 10.0
+        out = blur.output.eval({data: tmp})
+        scipy_out = scipy_filter(tmp, 20.0, mode='nearest')
+        np.testing.assert_allclose(out, scipy_out)
+
+    def test_no_blur(self):
+        theano.config.floatX = 'float64'
+        data = T.matrix('data')
+        data.tag.test_value = np.random.randn(10, 10)
+        blur = Blur(data, sigma=0.0, window_radius = 80)
+
+        tmp = np.random.randn(1000, 2000)
+        tmp += 10.0
+        out = blur.output.eval({data: tmp})
+#        scipy_out = scipy_filter(tmp, 20.0, mode='nearest')
+        np.testing.assert_allclose(out, tmp)
+
+
+class TestCenterBias(object):
+    def test_centerbias_ones(self):
+        theano.config.floatX = 'float64'
+        data = T.matrix('data')
+        data.tag.test_value = np.random.randn(10, 10)
+        center_bias = CenterBias(data)
+
+        tmp = np.ones((1000, 2000))
+        out = center_bias.output.eval({data: tmp})
+        np.testing.assert_allclose(out, tmp)
+
+    def test_centerbias_random(self):
+        theano.config.floatX = 'float64'
+        data = T.matrix('data')
+        data.tag.test_value = np.random.randn(10, 10)
+        center_bias = CenterBias(data)
+
+        tmp = np.random.randn(1000, 2000)
+        out = center_bias.output.eval({data: tmp})
+        np.testing.assert_allclose(out, tmp)
+
+    def test_centerbias_empty(self):
+        theano.config.floatX = 'float64'
+        data = T.matrix('data')
+        data.tag.test_value = np.random.randn(10, 10)
+        center_bias = CenterBias(data, centerbias = np.array([1.0]), alpha=3.0)
+
+        tmp = np.random.randn(1000, 2000)
+        out = center_bias.output.eval({data: tmp})
+        np.testing.assert_allclose(out, tmp)
