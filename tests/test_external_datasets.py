bac00d0062ddae8de9729313dd04d532eaed1ec3
Matthias Kümmerer <matthias@matthias-k.org>
2017-09-18 20:37:50 +0200

    Started transition to pytest
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- tests/test_external_datasets.py
+++ tests/test_external_datasets.py
@@ -1,93 +1,114 @@
 from __future__ import absolute_import, print_function, division
 
+import pytest
+
 import unittest
-import os.path
 
 import pysaliency
 import pysaliency.external_datasets
-from test_helpers import TestWithData
 
 
-class TestExternalDatasets(TestWithData):
-    def test_toronto_without_location(self):
-        stimuli, fixations = pysaliency.external_datasets.get_toronto(location=None)
-        self.assertIsInstance(stimuli, pysaliency.Stimuli)
-        self.assertNotIsInstance(stimuli, pysaliency.FileStimuli)
-
-        self.assertEqual(len(stimuli.stimuli), 120)
-        for n in range(len(stimuli.stimuli)):
-            self.assertEqual(stimuli.shapes[n], (511, 681, 3))
-            self.assertEqual(stimuli.sizes[n], (511, 681))
-
-    def test_toronto_with_location(self):
-        stimuli, fixations = pysaliency.external_datasets.get_toronto(location=self.data_path)
-        self.assertIsInstance(stimuli, pysaliency.Stimuli)
-        self.assertIsInstance(stimuli, pysaliency.FileStimuli)
-        self.assertTrue(os.path.isdir(os.path.join(self.data_path, 'toronto')))
-
-        self.assertEqual(len(stimuli.stimuli), 120)
-        for n in range(len(stimuli.stimuli)):
-            self.assertEqual(stimuli.shapes[n], (511, 681, 3))
-            self.assertEqual(stimuli.sizes[n], (511, 681))
-
-    def test_mit_without_location(self):
-        stimuli, fixations = pysaliency.external_datasets.get_mit1003(location=None)
-        self.assertIsInstance(stimuli, pysaliency.Stimuli)
-        self.assertNotIsInstance(stimuli, pysaliency.FileStimuli)
-
-        self.assertEqual(len(stimuli.stimuli), 1003)
-        for n in range(len(stimuli.stimuli)):
-            self.assertEqual(max(stimuli.sizes[n]), 1024)
-
-        self.assertEqual(len(fixations.x), 104171)
-        self.assertEqual((fixations.n == 0).sum(), 121)
-
-    def test_mit_with_location(self):
-        stimuli, fixations = pysaliency.external_datasets.get_mit1003(location=self.data_path)
-        self.assertIsInstance(stimuli, pysaliency.Stimuli)
-        self.assertIsInstance(stimuli, pysaliency.FileStimuli)
-        self.assertTrue(os.path.isdir(os.path.join(self.data_path, 'MIT1003')))
-
-        self.assertEqual(len(stimuli.stimuli), 1003)
-        for n in range(len(stimuli.stimuli)):
-            self.assertEqual(max(stimuli.sizes[n]), 1024)
-
-        self.assertEqual(len(fixations.x), 104171)
-        self.assertEqual((fixations.n == 0).sum(), 121)
-
-    def test_mit_onesize_without_location(self):
-        stimuli, fixations = pysaliency.external_datasets.get_mit1003_onesize(location=None)
-        self.assertIsInstance(stimuli, pysaliency.Stimuli)
-        self.assertNotIsInstance(stimuli, pysaliency.FileStimuli)
-
-        self.assertEqual(len(stimuli.stimuli), 463)
-        for n in range(len(stimuli.stimuli)):
-            self.assertEqual(stimuli.sizes[n], (768, 1024))
-
-        self.assertEqual(len(fixations.x), 48771)
-        self.assertEqual((fixations.n == 0).sum(), 121)
-
-    def test_mit_onesize_with_location(self):
-        stimuli, fixations = pysaliency.external_datasets.get_mit1003_onesize(location=self.data_path)
-        self.assertIsInstance(stimuli, pysaliency.Stimuli)
-        self.assertIsInstance(stimuli, pysaliency.FileStimuli)
-        self.assertTrue(os.path.isdir(os.path.join(self.data_path, 'MIT1003_onesize')))
-
-        self.assertEqual(len(stimuli.stimuli), 463)
-        for n in range(len(stimuli.stimuli)):
-            self.assertEqual(stimuli.sizes[n], (768, 1024))
-
-        self.assertEqual(len(fixations.x), 48771)
-        self.assertEqual((fixations.n == 0).sum(), 121)
-
-
-class TestExternalDatasetsOctave(TestExternalDatasets):
-    def setUp(self):
-        self.old_matlab_names = pysaliency.utils.MatlabOptions
+@pytest.fixture(params=["matlab", "octave"])
+def matlab(request):
+    if request.param == "matlab":
+        pysaliency.utils.MatlabOptions.matlab_names = ['matlab', 'matlab.exe']
+        pysaliency.utils.MatlabOptions.octave_names = []
+    elif request.param == 'octave':
         pysaliency.utils.MatlabOptions.matlab_names = []
+        pysaliency.utils.MatlabOptions.octave_names = ['octave', 'octave.exe']
 
-    def tearDown(self):
-        pysaliency.utils.MatlabOptions.matlab_names = self.old_matlab_names
+    return request.param
+
+
+@pytest.fixture(autouse=True)
+def skip_by_matlab(request, matlab):
+    if request.node.get_marker('skip_octave'):
+        if matlab == 'octave':
+            pytest.skip('skipped octave')
+
+
+@pytest.fixture(params=["no_location", "with_location"])
+def location(tmpdir, request):
+    if request.param == 'no_location':
+        return None
+    elif request.param == 'with_location':
+        return tmpdir
+    else:
+        raise ValueError(request.param)
+
+
+def _location(location):
+    if location is not None:
+        return str(location)
+    return location
+
+
+@pytest.mark.slow
+@pytest.mark.download
+def test_toronto(location):
+    real_location = _location(location)
+
+    stimuli, fixations = pysaliency.external_datasets.get_toronto(location=real_location)
+    if location is None:
+        assert isinstance(stimuli, pysaliency.Stimuli)
+        assert not isinstance(stimuli, pysaliency.FileStimuli)
+    else:
+        assert isinstance(stimuli, pysaliency.FileStimuli)
+        assert location.join('toronto/stimuli.pydat').check()
+        assert location.join('toronto/fixations.pydat').check()
+
+    assert len(stimuli.stimuli) == 120
+    for n in range(len(stimuli.stimuli)):
+        assert stimuli.shapes[n] == (511, 681, 3)
+        assert stimuli.sizes[n] == (511, 681)
+
+
+@pytest.mark.slow
+@pytest.mark.download
+@pytest.mark.skip_octave
+def test_mit1003(location, matlab):
+    real_location = _location(location)
+
+    stimuli, fixations = pysaliency.external_datasets.get_mit1003(location=real_location)
+
+    if location is None:
+        assert isinstance(stimuli, pysaliency.Stimuli)
+        assert not isinstance(stimuli, pysaliency.FileStimuli)
+    else:
+        assert isinstance(stimuli, pysaliency.FileStimuli)
+        assert location.join('MIT1003/stimuli.pydat').check()
+        assert location.join('MIT1003/fixations.pydat').check()
+
+    assert len(stimuli.stimuli) == 1003
+    for n in range(len(stimuli.stimuli)):
+        assert max(stimuli.sizes[n]) == 1024
+
+    assert len(fixations.x) == 104171
+    assert (fixations.n == 0).sum() == 121
+
+
+@pytest.mark.slow
+@pytest.mark.download
+@pytest.mark.skip_octave
+def test_mit1003_onesize(location, matlab):
+    real_location = _location(location)
+
+    stimuli, fixations = pysaliency.external_datasets.get_mit1003_onesize(location=real_location)
+
+    if location is None:
+        assert isinstance(stimuli, pysaliency.Stimuli)
+        assert not isinstance(stimuli, pysaliency.FileStimuli)
+    else:
+        assert isinstance(stimuli, pysaliency.FileStimuli)
+        assert location.join('MIT1003_onesize/stimuli.pydat').check()
+        assert location.join('MIT1003_onesize/fixations.pydat').check()
+
+    assert len(stimuli.stimuli) == 463
+    for n in range(len(stimuli.stimuli)):
+        assert stimuli.sizes[n] == (768, 1024)
+
+    assert len(fixations.x) == 48771
+    assert (fixations.n == 0).sum() == 121
 
 
 if __name__ == '__main__':
--- tests/test_saliency_map_conversion.py
+++ tests/test_saliency_map_conversion.py
@@ -1,11 +1,7 @@
 import numpy as np
 import dill
 
-from nose.tools import assert_equal
-
-from pysaliency import SaliencyMapConvertor, SaliencyMapModel
-
-from test_helpers import TestWithData
+from pysaliency import SaliencyMapConvertor, SaliencyMapModel, Stimuli, Fixations, optimize_for_information_gain
 
 
 class GaussianSaliencyMapModel(SaliencyMapModel):
@@ -18,23 +14,52 @@ class GaussianSaliencyMapModel(SaliencyMapModel):
         return np.ones((stimulus.shape[0], stimulus.shape[1]))*np.exp(-0.5*(r_squared/size))
 
 
-class TestSaliencyMapConvertor(TestWithData):
-    def test_pickle(self):
-        import theano
-        theano.config.floatX = 'float64'
-        model = GaussianSaliencyMapModel()
-        smc = SaliencyMapConvertor(model)
-        smc.set_params(nonlinearity=np.ones(20),
-                       centerbias = np.ones(12)*2,
-                       alpha=3,
-                       blur_radius=4,
-                       saliency_min=5,
-                       saliency_max=6)
+def test_optimize_for_IG():
+    model = GaussianSaliencyMapModel()
+    stimulus = np.random.randn(100, 100, 3)
+    stimuli = Stimuli([stimulus])
 
-        smc2 = self.pickle_and_reload(smc, pickler=dill)
-        np.testing.assert_allclose(smc2.saliency_map_processing.nonlinearity_ys.get_value(), np.ones(20))
-        np.testing.assert_allclose(smc2.saliency_map_processing.centerbias_ys.get_value(), np.ones(12)*2)
-        np.testing.assert_allclose(smc2.saliency_map_processing.alpha.get_value(), 3)
-        np.testing.assert_allclose(smc2.saliency_map_processing.blur_radius.get_value(), 4)
-        np.testing.assert_allclose(smc2.saliency_min, 5)
-        np.testing.assert_allclose(smc2.saliency_max, 6)
+    rst = np.random.RandomState(seed=42)
+    N = 100000
+    fixations = Fixations.create_without_history(
+        x = rst.rand(N)*100,
+        y = rst.rand(N)*100,
+        n = np.zeros(N, dtype=int)
+    )
+
+    smc = optimize_for_information_gain(
+        model,
+        stimuli,
+        fixations,
+        blur_radius=3,
+        verbose=2,
+        maxiter=10)
+
+    assert smc
+
+
+def test_saliency_map_converter(tmpdir):
+    import theano
+    theano.config.floatX = 'float64'
+    model = GaussianSaliencyMapModel()
+    smc = SaliencyMapConvertor(model)
+    smc.set_params(nonlinearity=np.ones(20),
+                   centerbias = np.ones(12)*2,
+                   alpha=3,
+                   blur_radius=4,
+                   saliency_min=5,
+                   saliency_max=6)
+
+    pickle_file = tmpdir.join('object.pydat')
+    with pickle_file.open(mode='wb') as f:
+        dill.dump(smc, f)
+
+    with pickle_file.open(mode='rb') as f:
+        smc2 = dill.load(f)
+
+    np.testing.assert_allclose(smc2.saliency_map_processing.nonlinearity_ys.get_value(), np.ones(20))
+    np.testing.assert_allclose(smc2.saliency_map_processing.centerbias_ys.get_value(), np.ones(12)*2)
+    np.testing.assert_allclose(smc2.saliency_map_processing.alpha.get_value(), 3)
+    np.testing.assert_allclose(smc2.saliency_map_processing.blur_radius.get_value(), 4)
+    np.testing.assert_allclose(smc2.saliency_min, 5)
+    np.testing.assert_allclose(smc2.saliency_max, 6)
