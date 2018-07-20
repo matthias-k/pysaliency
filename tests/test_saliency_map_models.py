6d90ed5333eeba53cf2219f6a879268695daebc4
Matthias Kümmerer <matthias@matthias-k.org>
2018-07-20 18:58:56 +0000

    Convert test_saliency_models to pytest
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- tests/test_saliency_map_models.py
+++ tests/test_saliency_map_models.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import pytest
+
 import numpy as np
 
 import pysaliency
@@ -35,192 +37,156 @@ class MixedSaliencyMapModel(pysaliency.SaliencyMapModel):
             return self.gaussian_model.saliency_map(stimulus)
 
 
-class TestAUC(object):
-    def setUp(self):
-        xs_trains = [
-            [0, 1, 2],
-            [2, 2],
-            [1, 5, 3]]
-        ys_trains = [
-            [10, 11, 12],
-            [12, 12],
-            [21, 25, 33]]
-        ts_trains = [
-            [0, 200, 600],
-            [100, 400],
-            [50, 500, 900]]
-        ns = [0, 0, 1]
-        subjects = [0, 1, 1]
-        self.f = pysaliency.FixationTrains.from_fixation_trains(xs_trains, ys_trains, ts_trains, ns, subjects)
-
-    def test_constant(self):
-        stimuli = pysaliency.Stimuli([np.random.randn(600, 1000, 3),
-                                      np.random.randn(600, 1000, 3)])
-        csmm = ConstantSaliencyMapModel()
-
-        aucs = csmm.AUCs(stimuli, self.f, nonfixations='uniform')
-        np.testing.assert_allclose(aucs, np.ones(len(self.f.x))*0.5)
-
-        aucs = csmm.AUCs(stimuli, self.f, nonfixations='shuffled')
-        np.testing.assert_allclose(aucs, np.ones(len(self.f.x))*0.5)
-
-        auc = csmm.AUC(stimuli, self.f, nonfixations=self.f)
-        np.testing.assert_allclose(auc, 0.5)
-
-    def test_gauss(self):
-        stimuli = pysaliency.Stimuli([np.random.randn(40, 40, 3),
-                                      np.random.randn(40, 40, 3)])
-        gsmm = GaussianSaliencyMapModel()
-
-        aucs = gsmm.AUCs(stimuli, self.f, nonfixations='uniform')
-        np.testing.assert_allclose(aucs, [0.099375,  0.158125,  0.241875,  0.241875,
-                                          0.241875,  0.291875, 0.509375,  0.138125],
-                                   rtol=1e-6)
-
-        aucs = gsmm.AUCs(stimuli, self.f, nonfixations='shuffled')
-        np.testing.assert_allclose(aucs, [0.0,         0.33333333,  0.33333333,  0.33333333,
-                                          0.33333333,  1.,          1.,          0.2],
-                                   rtol=1e-6)
-
-        auc = gsmm.AUC(stimuli, self.f, nonfixations=self.f)
-        np.testing.assert_allclose(auc, 0.5)
+@pytest.fixture
+def fixation_trains():
+    xs_trains = [
+        [0, 1, 2],
+        [2, 2],
+        [1, 5, 3]]
+    ys_trains = [
+        [10, 11, 12],
+        [12, 12],
+        [21, 25, 33]]
+    ts_trains = [
+        [0, 200, 600],
+        [100, 400],
+        [50, 500, 900]]
+    ns = [0, 0, 1]
+    subjects = [0, 1, 1]
+    return pysaliency.FixationTrains.from_fixation_trains(xs_trains, ys_trains, ts_trains, ns, subjects)
 
 
-        aucs_single = pysaliency.GeneralSaliencyMapModel.AUCs(gsmm, stimuli, self.f)
-        aucs_combined = gsmm.AUCs(stimuli, self.f, nonfixations='uniform')
-        np.testing.assert_allclose(aucs_single, aucs_combined)
+@pytest.fixture
+def stimuli():
+    return pysaliency.Stimuli([np.random.randn(40, 40, 3),
+                               np.random.randn(40, 40, 3)])
 
 
-class TestFixationBasedKLDivergence(object):
-    def setUp(self):
-        xs_trains = [
-            [0, 1, 2],
-            [2, 2],
-            [1, 5, 3]]
-        ys_trains = [
-            [10, 11, 12],
-            [12, 12],
-            [21, 25, 33]]
-        ts_trains = [
-            [0, 200, 600],
-            [100, 400],
-            [50, 500, 900]]
-        ns = [0, 0, 1]
-        subjects = [0, 1, 1]
-        self.f = pysaliency.FixationTrains.from_fixation_trains(xs_trains, ys_trains, ts_trains, ns, subjects)
-
-    def test_constant(self):
-        stimuli = pysaliency.Stimuli([np.random.randn(600, 1000, 3),
-                                      np.random.randn(600, 1000, 3)])
-        csmm = ConstantSaliencyMapModel()
-
-        fb_kl = csmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations='uniform')
-        np.testing.assert_allclose(fb_kl, 0.0)
-
-        fb_kl = csmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations='shuffled')
-        np.testing.assert_allclose(fb_kl, 0.0)
-
-        fb_kl = csmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations=self.f)
-        np.testing.assert_allclose(fb_kl, 0.0)
-
-    def test_gauss(self):
-        stimuli = pysaliency.Stimuli([np.random.randn(40, 40, 3),
-                                      np.random.randn(40, 40, 3)])
-        gsmm = GaussianSaliencyMapModel()
-
-        fb_kl = gsmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations='uniform')
-        np.testing.assert_allclose(fb_kl, 0.4787711930295902)
-
-        fb_kl = gsmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations='shuffled')
-        np.testing.assert_allclose(fb_kl, 0.0)
-
-        fb_kl = gsmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations=self.f)
-        np.testing.assert_allclose(fb_kl, 0.0)
-
-    def test_mixed(self):
-        stimuli = pysaliency.Stimuli([np.random.randn(40, 40, 3),
-                                      np.random.randn(40, 40, 3)])
-        gsmm = MixedSaliencyMapModel()
-
-        fb_kl = gsmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations='uniform')
-        np.testing.assert_allclose(fb_kl, 0.19700844437943388)
-
-        fb_kl = gsmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations='shuffled')
-        np.testing.assert_allclose(fb_kl, 5.874655219107867)
-
-        fb_kl = gsmm.fixation_based_KL_divergence(stimuli, self.f, nonfixations=self.f)
-        np.testing.assert_allclose(fb_kl, 0.0)
+@pytest.fixture
+def more_fixation_trains():
+    xs_trains = [
+        [0, 1, 2],
+        [2, 2],
+        [1, 10, 3],
+        [4, 5, 33, 7]]
+    ys_trains = [
+        [10, 11, 12],
+        [12, 12],
+        [21, 25, 33],
+        [41, 42, 43, 44]]
+    ts_trains = [
+        [0, 200, 600],
+        [100, 400],
+        [50, 500, 900],
+        [0, 1, 2, 3]]
+    ns = [0, 0, 1, 2]
+    subjects = [0, 1, 1, 0]
+    return pysaliency.FixationTrains.from_fixation_trains(xs_trains, ys_trains, ts_trains, ns, subjects)
 
 
-class TestImageBasedKLDivergence(object):
-    def setUp(self):
-        xs_trains = [
-            [0, 1, 2],
-            [2, 2],
-            [1, 5, 3]]
-        ys_trains = [
-            [10, 11, 12],
-            [12, 12],
-            [21, 25, 33]]
-        ts_trains = [
-            [0, 200, 600],
-            [100, 400],
-            [50, 500, 900]]
-        ns = [0, 0, 1]
-        subjects = [0, 1, 1]
-        self.f = pysaliency.FixationTrains.from_fixation_trains(xs_trains, ys_trains, ts_trains, ns, subjects)
-
-    def test_gauss(self):
-        stimuli = pysaliency.Stimuli([np.random.randn(40, 40, 3),
-                                      np.random.randn(40, 40, 3)])
-        gsmm = GaussianSaliencyMapModel()
-        constant_gold = ConstantSaliencyMapModel()
-        gold = pysaliency.FixationMap(stimuli, self.f, kernel_size = 10)
-
-        ib_kl = gsmm.image_based_kl_divergence(stimuli, gsmm)
-        np.testing.assert_allclose(ib_kl, 0.0)
-
-        ib_kl = gsmm.image_based_kl_divergence(stimuli, constant_gold)
-        np.testing.assert_allclose(ib_kl, 0.8152272380729648)
-
-        ib_kl = gsmm.image_based_kl_divergence(stimuli, gold)
-        np.testing.assert_allclose(ib_kl, 1.961124862592289)
-
-        ib_kl = gold.image_based_kl_divergence(stimuli, gold)
-        np.testing.assert_allclose(ib_kl, 0.0)
+@pytest.fixture
+def more_stimuli():
+    return pysaliency.Stimuli([np.random.randn(50, 50, 3),
+                               np.random.randn(50, 50, 3),
+                               np.random.randn(100, 200, 3)])
 
 
-class TestFullShuffledNonfixationProvider(object):
-    def setUp(self):
-        xs_trains = [
-            [0, 1, 2],
-            [2, 2],
-            [1, 10, 3],
-            [4, 5, 33, 7]]
-        ys_trains = [
-            [10, 11, 12],
-            [12, 12],
-            [21, 25, 33],
-            [41, 42, 43, 44]]
-        ts_trains = [
-            [0, 200, 600],
-            [100, 400],
-            [50, 500, 900],
-            [0, 1, 2, 3]]
-        ns = [0, 0, 1, 2]
-        subjects = [0, 1, 1, 0]
-        self.f = pysaliency.FixationTrains.from_fixation_trains(xs_trains, ys_trains, ts_trains, ns, subjects)
-        self.stimuli = pysaliency.Stimuli([np.random.randn(50, 50, 3),
-                                           np.random.randn(50, 50, 3),
-                                           np.random.randn(100, 200, 3)])
+def test_auc_constant(stimuli, fixation_trains):
+    csmm = ConstantSaliencyMapModel()
 
-    def test_shuffled(self):
-        from pysaliency.saliency_map_models import FullShuffledNonfixationProvider
-        prov = FullShuffledNonfixationProvider(self.stimuli, self.f)
+    aucs = csmm.AUCs(stimuli, fixation_trains, nonfixations='uniform')
+    np.testing.assert_allclose(aucs, np.ones(len(fixation_trains.x))*0.5)
+
+    aucs = csmm.AUCs(stimuli, fixation_trains, nonfixations='shuffled')
+    np.testing.assert_allclose(aucs, np.ones(len(fixation_trains.x))*0.5)
+
+    auc = csmm.AUC(stimuli, fixation_trains, nonfixations=fixation_trains)
+    np.testing.assert_allclose(auc, 0.5)
 
 
-        xs, ys = prov(self.stimuli, self.f, 0)
-        assert len(xs) == (self.f.n != 0).sum()
-        np.testing.assert_allclose(xs, [1, 10, 3, 1, 1, 8, 1])
-        np.testing.assert_allclose(ys, [21, 25, 33, 20, 21, 21, 22])
+def test_auc_gauss(stimuli, fixation_trains):
+    gsmm = GaussianSaliencyMapModel()
 
+    aucs = gsmm.AUCs(stimuli, fixation_trains, nonfixations='uniform')
+    np.testing.assert_allclose(aucs, [0.099375,  0.158125,  0.241875,  0.241875,
+                                      0.241875,  0.291875, 0.509375,  0.138125],
+                               rtol=1e-6)
+
+    aucs = gsmm.AUCs(stimuli, fixation_trains, nonfixations='shuffled')
+    np.testing.assert_allclose(aucs, [0.0,         0.33333333,  0.33333333,  0.33333333,
+                                      0.33333333,  1.,          1.,          0.2],
+                               rtol=1e-6)
+
+    auc = gsmm.AUC(stimuli, fixation_trains, nonfixations=fixation_trains)
+    np.testing.assert_allclose(auc, 0.5)
+
+    aucs_single = pysaliency.GeneralSaliencyMapModel.AUCs(gsmm, stimuli, fixation_trains)
+    aucs_combined = gsmm.AUCs(stimuli, fixation_trains, nonfixations='uniform')
+    np.testing.assert_allclose(aucs_single, aucs_combined)
+
+
+def test_fixation_based_kldiv_constant(stimuli, fixation_trains):
+    csmm = ConstantSaliencyMapModel()
+
+    fb_kl = csmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations='uniform')
+    np.testing.assert_allclose(fb_kl, 0.0)
+
+    fb_kl = csmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations='shuffled')
+    np.testing.assert_allclose(fb_kl, 0.0)
+
+    fb_kl = csmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations=fixation_trains)
+    np.testing.assert_allclose(fb_kl, 0.0)
+
+
+def test_fixation_based_kldiv_gauss(stimuli, fixation_trains):
+    gsmm = GaussianSaliencyMapModel()
+
+    fb_kl = gsmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations='uniform')
+    np.testing.assert_allclose(fb_kl, 0.4787711930295902)
+
+    fb_kl = gsmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations='shuffled')
+    np.testing.assert_allclose(fb_kl, 0.0)
+
+    fb_kl = gsmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations=fixation_trains)
+    np.testing.assert_allclose(fb_kl, 0.0)
+
+
+def test_fixation_based_kldiv_mixed(stimuli, fixation_trains):
+    gsmm = MixedSaliencyMapModel()
+
+    fb_kl = gsmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations='uniform')
+    np.testing.assert_allclose(fb_kl, 0.19700844437943388)
+
+    fb_kl = gsmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations='shuffled')
+    np.testing.assert_allclose(fb_kl, 5.874655219107867)
+
+    fb_kl = gsmm.fixation_based_KL_divergence(stimuli, fixation_trains, nonfixations=fixation_trains)
+    np.testing.assert_allclose(fb_kl, 0.0)
+
+
+def test_image_based_kldiv_gauss(stimuli, fixation_trains):
+    gsmm = GaussianSaliencyMapModel()
+    constant_gold = ConstantSaliencyMapModel()
+    gold = pysaliency.FixationMap(stimuli, fixation_trains, kernel_size = 10)
+
+    ib_kl = gsmm.image_based_kl_divergence(stimuli, gsmm)
+    np.testing.assert_allclose(ib_kl, 0.0)
+
+    ib_kl = gsmm.image_based_kl_divergence(stimuli, constant_gold)
+    np.testing.assert_allclose(ib_kl, 0.8152272380729648)
+
+    ib_kl = gsmm.image_based_kl_divergence(stimuli, gold)
+    np.testing.assert_allclose(ib_kl, 1.961124862592289)
+
+    ib_kl = gold.image_based_kl_divergence(stimuli, gold)
+    np.testing.assert_allclose(ib_kl, 0.0)
+
+
+def test_shuffled_nonfixation_provider(more_stimuli, more_fixation_trains):
+    from pysaliency.saliency_map_models import FullShuffledNonfixationProvider
+    prov = FullShuffledNonfixationProvider(more_stimuli, more_fixation_trains)
+
+    xs, ys = prov(more_stimuli, more_fixation_trains, 0)
+    assert len(xs) == (more_fixation_trains.n != 0).sum()
+    np.testing.assert_allclose(xs, [1, 10, 3, 1, 1, 8, 1])
+    np.testing.assert_allclose(ys, [21, 25, 33, 20, 21, 21, 22])
