f7ce42edc80f8bb37445a9e8364ecc51c7a87054
Matthias Kümmerer <matthias@matthias-k.org>
2019-12-05 01:16:04 +0100

    Bugfix
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- pysaliency/external_datasets.py
+++ pysaliency/external_datasets.py
@@ -609,7 +609,7 @@ def get_cat2000_test(location=None):
 def get_cat2000_train(location=None, include_initial_fixation=False):
     name = 'CAT2000_train'
 
-    if not include_initial_fixation:
+    if include_initial_fixation:
         name += 'with_initial_fixation'
 
     return _get_cat2000_train(name=name, location=location, include_initial_fixation=include_initial_fixation)
