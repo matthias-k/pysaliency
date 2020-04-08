c2a411c92f77c29923362387225fe8d9f68d6bf4
Matthias Kümmerer <matthias@matthias-k.org>
2020-04-08 17:13:07 +0200

    Speed up tests
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- tests/test_metric_optimization_torch.py
+++ tests/test_metric_optimization_torch.py
@@ -4,33 +4,34 @@ from pysaliency.metric_optimization_torch import maximize_expected_sim
 
 
 def test_maximize_expected_sim_decay_1overk():
-    density = np.ones((100, 100))
-    density[30:70, 40:60] = 20
-    density[10:15, 80:90] = 30
+    density = np.ones((20, 20))
+    density[6:17, 8:12] = 20
+    density[2:4, 18:18] = 30
     density /= density.sum()
     log_density = np.log(density)
 
     saliency_map, score = maximize_expected_sim(
         log_density=log_density,
-        kernel_size=5,
+        kernel_size=1,
         train_samples_per_epoch=1000,
         val_samples=1000,
         max_iter=100
     )
 
-    np.testing.assert_allclose(score, -0.8147147089242934, rtol=5e-7)  # need bigger tolerance to handle differences between CPU and GPU
+    print(score)
+    np.testing.assert_allclose(score, -0.8202784448862075, rtol=5e-7)  # need bigger tolerance to handle differences between CPU and GPU
 
 
 def test_maximize_expected_sim_decay_on_plateau():
-    density = np.ones((100, 100))
-    density[30:70, 40:60] = 20
-    density[10:15, 80:90] = 30
+    density = np.ones((20, 20))
+    density[6:17, 8:12] = 20
+    density[2:4, 18:18] = 30
     density /= density.sum()
     log_density = np.log(density)
 
     saliency_map, score = maximize_expected_sim(
         log_density=log_density,
-        kernel_size=5,
+        kernel_size=1,
         train_samples_per_epoch=1000,
         val_samples=1000,
         max_iter=100,
@@ -39,4 +40,4 @@ def test_maximize_expected_sim_decay_on_plateau():
         learning_rate_decay_scheme='validation_loss',
     )
 
-    np.testing.assert_allclose(score, -0.8154174327850342, rtol=5e-7)  # need bigger tolerance to handle differences between CPU and GPU
+    np.testing.assert_allclose(score, -0.8203513294458387, rtol=5e-7)  # need bigger tolerance to handle differences between CPU and GPU
