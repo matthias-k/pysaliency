caeab7bdb83429c3fef2fec1949ee685909a6dda
Harneet Singh Khanuja <hskhanuja2@gmail.com>
2023-10-25 14:02:39 +0200

    Updated numba functions (#28)
    reimplementing cython functions in numba so that we can avoid having to build cython code all the time,
    
    ---------
    
    Co-authored-by: Harneet Singh Khanuja <harneet.tubingen@gmail.com>

--- pysaliency/numba_utils.py
+++ pysaliency/numba_utils.py
@@ -47,3 +47,88 @@ def _auc_for_one_positive(positive, negatives):
             count += 0.5
 
     return count / len(negatives)
+
+
+def general_roc_numba(positives, negatives, judd=0):
+    sorted_positives = np.sort(positives)[::-1]
+    sorted_negatives = np.sort(negatives)[::-1]
+
+    if judd == 0:
+        all_values = np.hstack([positives, negatives])
+        all_values = np.sort(all_values)[::-1]
+    else:
+        min_val = min(sorted_positives[len(positives)-1], sorted_negatives[len(negatives)-1])
+        max_val = max(sorted_positives[0], sorted_negatives[0]) + 1
+        all_values = np.hstack((max_val, positives, min_val))
+        all_values = np.sort(all_values)[::-1]
+
+    false_positive_rates = np.zeros(len(all_values) + 1)
+    hit_rates = np.zeros(len(all_values) + 1)
+    hit_rates, false_positive_rates = _general_roc_numba(all_values, sorted_positives, sorted_negatives, false_positive_rates, hit_rates)
+    auc = np.trapz(hit_rates, false_positive_rates)
+
+    return auc, hit_rates, false_positive_rates
+
+
+@numba.jit(nopython=True)
+def _general_roc_numba(all_values, sorted_positives, sorted_negatives, false_positive_rates, hit_rates):
+    """calculate ROC score for given values of positive and negative
+    distribution"""
+
+    positive_count = len(sorted_positives)
+    negative_count = len(sorted_negatives)
+    true_positive_count = 0
+    false_positive_count = 0
+    for i in range(len(all_values)):
+        theta = all_values[i]
+        while true_positive_count < positive_count and sorted_positives[true_positive_count] >= theta:
+            true_positive_count += 1
+        while false_positive_count < negative_count and sorted_negatives[false_positive_count] >= theta:
+            false_positive_count += 1
+        false_positive_rates[i+1] = float(false_positive_count) / negative_count
+        hit_rates[i+1] = float(true_positive_count) / positive_count
+
+    return hit_rates, false_positive_rates
+
+
+def general_rocs_per_positive_numba(positives, negatives):
+    sorted_positives = np.sort(positives)
+    sorted_negatives = np.sort(negatives)
+    sorted_inds = np.argsort(positives)
+
+    results = np.empty(len(positives))
+    results = _general_rocs_per_positive_numba(sorted_positives, sorted_negatives, sorted_inds, results)
+
+    return results
+
+
+@numba.jit(nopython=True)
+def _general_rocs_per_positive_numba(sorted_positives, sorted_negatives, sorted_inds, results):
+    """calculate ROC scores for each positive against a list of negatives
+    distribution. The mean over the result will equal the return value of `general_roc`."""
+
+    true_negatives_count = 0
+    equal_count = 0
+    last_theta = -np.inf
+    negative_count = len(sorted_negatives)
+
+    for i, theta in enumerate(sorted_positives):
+
+        if theta == last_theta:
+            results[sorted_inds[i]] = (1.0 * true_negatives_count + 0.5 * equal_count) / negative_count
+            continue
+
+        true_negatives_count = true_negatives_count + equal_count
+
+        while true_negatives_count < negative_count and sorted_negatives[true_negatives_count] < theta:
+            true_negatives_count += 1
+
+        equal_count = 0
+        while true_negatives_count + equal_count < negative_count and sorted_negatives[true_negatives_count + equal_count] <= theta:
+            equal_count += 1
+
+        results[sorted_inds[i]] = (1.0 * true_negatives_count + 0.5 * equal_count) / negative_count
+
+        last_theta = theta
+
+    return results
--- tests/test_numba_utils.py
+++ tests/test_numba_utils.py
@@ -1,8 +1,8 @@
-from hypothesis import given, strategies as st
+from hypothesis import given, strategies as st, assume, settings
 import numpy as np
 
-from pysaliency.numba_utils import auc_for_one_positive
-from pysaliency.roc import general_roc
+from pysaliency.numba_utils import auc_for_one_positive, general_roc_numba, general_rocs_per_positive_numba
+from pysaliency.roc_cython import general_roc, general_rocs_per_positive
 
 
 def test_auc_for_one_positive():
@@ -17,3 +17,37 @@ def test_simple_auc_hypothesis(negatives, positive):
     old_auc, _, _ = general_roc(np.array([positive]), np.array(negatives))
     new_auc = auc_for_one_positive(positive, np.array(negatives))
     np.testing.assert_allclose(old_auc, new_auc)
+
+
+@settings(deadline=None)        #to remove time limit from a test
+@given(st.lists(st.floats(allow_infinity=False,allow_nan=False),min_size=1), st.lists(st.floats(allow_infinity=False,allow_nan=False),min_size=1))
+def test_numba_auc_test1(positives,negatives):
+    positives = np.array(positives)
+    negatives = np.array(negatives)
+    numba_output = general_roc_numba(positives,negatives)
+    cython_output = general_roc(positives,negatives)
+    assert np.isclose(numba_output[0],cython_output[0])
+    assert (numba_output[1] == cython_output[1]).all()
+    assert (numba_output[2] == cython_output[2]).all()
+
+
+@settings(deadline=None)
+@given(st.lists(st.floats(allow_infinity=False,allow_nan=False),min_size=1), st.floats(allow_infinity=False,allow_nan=False))
+def test_numba_auc_test2(positives,temp_variable):
+    positives = np.array(positives)
+    negatives = positives+temp_variable
+    numba_output = general_roc_numba(positives,negatives)
+    cython_output = general_roc(positives,negatives)
+    assert np.isclose(numba_output[0],cython_output[0])
+    assert (numba_output[1] == cython_output[1]).all()
+    assert (numba_output[2] == cython_output[2]).all()
+
+
+@settings(deadline=None)
+@given(st.lists(st.floats(allow_infinity=False,allow_nan=False),min_size=1), st.lists(st.floats(allow_infinity=False,allow_nan=False),min_size=1))
+def test_numba_rocs_per_positive(positives,negatives):
+    positives = np.array(positives)
+    negatives = np.array(negatives)
+    numba_output = general_rocs_per_positive_numba(positives,negatives)
+    cython_output = general_rocs_per_positive(positives,negatives)
+    assert (numba_output == cython_output).all()
