3b6cfa117c752245a3f758a96855bd9f5c0679b3
Matthias Kümmerer <matthias@matthias-k.org>
2025-01-23 20:11:34 +0100

    chore: remove obsolete workaround
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- pysaliency/optpy/optimization.py
+++ pysaliency/optpy/optimization.py
@@ -22,33 +22,6 @@ class LinearConstraint(object):
         self.keep_feasible = keep_feasible
 
 
-# TODO: Remove, once https://github.com/scipy/scipy/pull/11872 is merged and published
-class MemoizeJac(object):
-    """ Decorator that caches the value and gradient of function each time it
-    is called. """
-
-    def __init__(self, fun):
-        self.fun = fun
-        self.jac = None
-        self.value = None
-        self.x = None
-
-    def compute_if_needed(self, x, *args):
-        if self.value is None or self.jac is None or not np.all(x == self.x):
-            self.x = np.asarray(x).copy()
-            fg = self.fun(x, *args)
-            self.jac = fg[1]
-            self.value = fg[0]
-
-    def __call__(self, x, *args):
-        self.compute_if_needed(x, *args)
-        return self.value
-
-    def derivative(self, x, *args):
-        self.compute_if_needed(x, *args)
-        return self.jac
-
-
 class ParameterManager(object):
     def __init__(self, parameters, optimize, **kwargs):
         """ Create a parameter manager
@@ -229,11 +202,6 @@ def minimize(f, parameter_manager_or_x0, optimize=None, args=(), kwargs=None, me
         fun = jac_approx(wrapped_f, 1e-8)
         jac_ = fun.jac
         fun_ = fun.func
-
-    # TODO: Remove once https://github.com/scipy/scipy/pull/11872 is merged
-    if jac_ is True:
-        fun_ = MemoizeJac(fun_)
-        jac_ = fun_.derivative
 
     # Adapt constraints
     if isinstance(constraints, dict):
